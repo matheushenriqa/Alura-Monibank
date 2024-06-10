09e72329eb901f22c77c8d970925b4c998f402bf
Matheus Henrique <matheus-cd@hotmail.com>
2024-06-09 23:08:59 -0300

    Primeiro commit

---
+++ js/valida-cpf.js
@@ -0,0 +1,63 @@
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    
+    if (validaNumerosRepetidos(cpf) || validaPrimeroNumero(cpf) || validaSegundoNumero(cpf)) {
+        campo.setCustomValidity('Esse CPF é inválido');
+    }
+}
+
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeroNumero(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    console.log(soma != cpf[9]);
+
+    return soma != cpf[9];
+}
+
+function validaSegundoNumero(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+    console.log(soma != cpf[10]);
+
+    return soma != cpf[10];
+}
---
+++ js/valida-idade.js
@@ -0,0 +1,13 @@
+export default function ehMaiorDeIdade(campo) {
+    const dataNascimento = new Date(campo.value);
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('Você deve ser maior que 18 anos para se cadastrar.');
+    }
+}
+
+function validaIdade(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+    
+    return dataAtual >= dataMais18;
+}
